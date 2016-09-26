6e7acac21f394d1828467365896e197ce164ca95
NateATXCode <nathan.brigmon@austintexas.gov>
2016-09-26 12:55:04 -0500

    updating new static pages, also added js file

---
+++ js/custom-script.js
@@ -0,0 +1,15 @@
+/*!
+ * custom javascript script for github page
+ */
+
+
+$(document).ready(function() {
+  'use strict';
+  
+	var dateString = "Oct 2016";
+	console.log('yo');
+
+	console.log("hello!");
+	$("#dateString").html(dateString);
+
+ });
