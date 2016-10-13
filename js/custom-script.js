0c18e17cd77d6720bb2b11e368b58411a62353a4
NateATXCode <nathan.brigmon@austintexas.gov>
2016-10-13 16:18:01 -0500

    new arial black font for headers

--- js/custom-script.js
+++ js/custom-script.js
@@ -5,10 +5,13 @@
 
 $(document).ready(function() {
   'use strict';
-  
+
 	var dateString = "Oct 2016";
-	
+
 	console.log("hi! welcome to austin code's github!!!");
-	$("#dateString").html(dateString);
+	$("#footnoteInsert").html(`
+      <span class="label label-default">Last Update:
+        <span id="dateString">`+dateString+`</span>
+      </span>`);
 
  });
