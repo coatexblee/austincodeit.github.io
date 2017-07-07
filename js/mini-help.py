069f40cf175537dd3b4b9a4669c498e3a0f591af
Brigmon <Nathan.Brigmon@austintexas.gov>
2017-07-07 14:56:44 -0500

    added minification helper

---
+++ js/mini-help.py
@@ -0,0 +1,12 @@
+from jsmin import jsmin
+
+filesToMinify = ['str-app','rop-app','route-app','custom-script','load-task-list','print-pdf']
+
+for filename in filesToMinify:
+	newName = filename+'.min.js'
+	print newName
+	with open(filename+'.js') as js_file:
+		f = open(newName,"w+")
+		minified = jsmin(js_file.read())
+		f.write(minified)
+		f.close()
